8146034c79de3fca9a3928df08ce3890bc3af2a4
SteefCoder <stefanvanderduijs@icloud.com>
2025-12-31 19:35:32 +0100

    Added route

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 from config import Config
 
+from app.routes import create_hello_view
 
 def create_app(config_class=Config):
     app = Flask(__name__)
 
     app.config.from_object(config_class)
+
+    create_hello_view(app)
 
     return app
