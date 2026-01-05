200826a0db9197df47a05290b5a52e9eb4757d8e
SteefCoder <stefanvanderduijs@icloud.com>
2026-01-05 14:41:29 +0100

    Add a simple route

--- app/knsb/routes.py
+++ app/knsb/routes.py
@@ -1,1 +1,11 @@
 from . import bp
+
+from app.models import KnsbPlayer, db
+
+from sqlalchemy import select
+
+
+@bp.route('/players')
+def players():
+    query = select(KnsbPlayer).limit(10)
+    return [x._tuple()[0].asdict() for x in db.session.execute(query).all()]
