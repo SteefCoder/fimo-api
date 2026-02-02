5d7ad2e4677db897f3053d1cabfb7cc544dec62e
SteefCoder <stefanvanderduijs@icloud.com>
2026-02-02 19:50:05 +0100

    Added defaults for meta

--- app/database/fide/meta.py
+++ app/database/fide/meta.py
@@ -9,35 +9,34 @@ import pandas as pd
 meta_file = pathlib.Path.cwd() / 'instance' / 'meta.json'
 
 
-def read_meta():
-    with meta_file.open() as f:
-        return json.load(f)
-
-
 @contextmanager
-def open_meta():
-    with meta_file.open() as f:
-        meta = json.load(f)
+def open_meta(mode: str = 'r'):
+    if meta_file.exists():
+        with meta_file.open(encoding='utf8') as f:
+            meta = json.load(f)
+    else:
+        meta = {}
     
     try:
         yield meta
     finally:
-        with meta_file.open('w', encoding='utf8') as f:
-            json.dump(meta, f, indent=2)
+        if mode == 'w':
+            with meta_file.open('w', encoding='utf8') as f:
+                json.dump(meta, f, indent=2)
 
 
 def today_iso() -> str:
     return datetime.date.today().isoformat()
 
 
-def write_player_meta(df: pd.DataFrame):
+def write_player_meta(df: pd.DataFrame) -> None:
     unrated = (
         df['standard_rating'].isna() &
         df['rapid_rating'].isna() &
         df['blitz_rating'].isna()
     ).sum()
 
-    with open_meta() as meta:
+    with open_meta('w') as meta:
         meta['fide-player'] = {
             'records': len(df),
             'last-updated': today_iso(),
@@ -46,8 +45,15 @@ def write_player_meta(df: pd.DataFrame):
         }
 
 
-def write_rating_meta(df: pd.DataFrame, date: datetime.date):
-    with open_meta() as meta:
+def write_rating_meta(df: pd.DataFrame, date: datetime.date) -> None:
+    with open_meta('w') as meta:
+        if 'fide-rating' not in meta:
+            meta['fide-rating'] = {}
+        if 'lists' not in meta['fide-rating']:
+            meta['fide-rating']['lists'] = []
+        if 'total-records' not in meta['fide-rating']:
+            meta['fide-rating']['total-records'] = 0
+
         meta['fide-rating']['lists'].append({
             'date': date,
             'records': len(df),
@@ -60,6 +66,9 @@ def write_rating_meta(df: pd.DataFrame, date: datetime.date):
         meta['fide-rating']['last-updated'] = today_iso()
 
 
-def rating_exists_meta(date: datetime.date):
-    meta = read_meta()
-    return date.isoformat() in {x['date'] for x in meta['fide-rating']['lists']}
+def rating_exists_meta(date: datetime.date) -> bool:
+    with open_meta() as meta:
+        if not 'fide-rating' in meta or not 'lists' in meta['fide-rating']:
+            return False
+        
+        return date.isoformat() in {x['date'] for x in meta['fide-rating']['lists']}
