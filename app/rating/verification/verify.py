14e878956aa32c75841d9d65c0594af59757893b
SteefCoder <stefanvanderduijs@icloud.com>
2026-02-13 14:59:47 +0100

    Extra verificatie

--- app/rating/verification/verify.py
+++ app/rating/verification/verify.py
@@ -20,10 +20,11 @@ def validate_speler(speler: Speler, repo: RatingRepository) -> Speler:
             raise VerificationError(f"Geen match tussen gegeven KNSB ID {knsb_id} en FIDE ID {fide_id}.")
     
     elif fide_id:
+        fide = repo.get_fide(fide_id)
         knsb = repo.get_knsb_from_fide(fide_id)
         if knsb:
             knsb_id = knsb.knsb_id
-    
+        
     return Speler(knsb_id, fide_id)
 
 
@@ -32,4 +33,6 @@ def validate_partijlijst(lijst: PartijLijstIn, repo: RatingRepository) -> Partij
     domain_lijst.speler = validate_speler(domain_lijst.speler, repo)
     for partij in domain_lijst.partijen:
         partij.tegenstander = validate_speler(partij.tegenstander, repo)
+        if partij.tegenstander == domain_lijst.speler:
+            raise VerificationError("Een speler kan niet tegen zichzelf spelen.")
     return domain_lijst
